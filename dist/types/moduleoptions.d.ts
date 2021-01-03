4f4df196a4ce155014d0dc2b7b19cb4bd8aa8b4d
Filip Š <filip.stamcar@hotmail.com>
2021-01-03 12:41:14 +0100

    Publish dist so I can use this package temporary until the PR is merged

---
+++ dist/types/moduleoptions.d.ts
@@ -0,0 +1,49 @@
+import { Store } from 'vuex';
+/**
+ * Options to pass to the @Module decorator
+ */
+export interface StaticModuleOptions {
+    /**
+     * name of module, if being namespaced
+     */
+    name?: string;
+    /**
+     * whether or not the module is namespaced
+     */
+    namespaced?: boolean;
+    /**
+     * Whether to generate a plain state object, or a state factory for the module
+     */
+    stateFactory?: boolean;
+}
+export interface DynamicModuleOptions {
+    /**
+     * If this is a dynamic module (added to store after store is created)
+     */
+    dynamic: true;
+    /**
+     * The store into which this module will be injected
+     */
+    store: Store<any>;
+    /**
+     * name of module, compulsory for dynamic modules
+     */
+    name: string;
+    /**
+     * If this is enabled it will preserve the existing state when loading the module
+     */
+    preserveState?: boolean;
+    /**
+     * If preserveState, specify how to deal with the default module state
+     */
+    preserveStateType?: 'always' | 'existing' | 'mergeReplaceArrays' | 'mergeConcatArrays';
+    /**
+     * Whether or not the module is namespaced
+     */
+    namespaced?: boolean;
+    /**
+     * Whether to generate a plain state object, or a state factory for the module
+     */
+    stateFactory?: boolean;
+}
+export declare type ModuleOptions = StaticModuleOptions | DynamicModuleOptions;
